fd40b719b5108fbea56f52860c58b7f398805590
jgPark <jeonggy1230@naver.com>
2025-05-20 14:03:58 +0900

    added character movement (left/right) and jump

---
+++ Assets/Scripts/RKnight_Script.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class RKnight_Script : MonoBehaviour
+{
+    float moveSpeed = 4.0f;
+    float jumpPower = 15.0f;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+    Rigidbody2D rigid;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+        this.animator = GetComponent<Animator>();
+        this.rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 좌, 우 움직임을 제어
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.spriteRenderer.flipX = true;
+            this.rigid.velocity = new Vector2(-this.moveSpeed, this.rigid.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.spriteRenderer.flipX = false;
+            this.rigid.velocity = new Vector2(this.moveSpeed, this.rigid.velocity.y);
+        }
+        else
+        {
+            this.rigid.velocity = new Vector2(0, this.rigid.velocity.y);
+        }
+
+        // 좌, 우 이동시 애니메이션 활상화, 비활성화
+        if (this.rigid.velocity.normalized.x == 0)
+        {
+            this.animator.SetBool("IsWalk", false);
+        }
+        else
+        {
+            this.animator.SetBool("IsWalk", true);
+        }
+
+        // jump를 제어
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+}
+
